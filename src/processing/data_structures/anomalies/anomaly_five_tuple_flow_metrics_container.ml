752b618f9fc266eb2872dd6ccf01f3fcb720149e
Johan Mazel <jmazel@johan-precision.r.nii.ac.jp>
2016-03-27 17:46:13 +0900

    modify fibe tuple flow and anomaly indice

--- src/processing/data_structures/anomalies/anomaly_five_tuple_flow_metrics_container.ml
+++ src/processing/data_structures/anomalies/anomaly_five_tuple_flow_metrics_container.ml
@@ -105,308 +105,88 @@ let add_packet_ethernet
     (* debug "add_packet_ethernet: call"; *)
 
     (* debug "add_packet_ethernet: building five_tuple_flow"; *)
-    
-    let five_tuple_flow = 
-      Five_tuple_flow.of_packet_data_for_metrics
-        packet_data_for_metrics
-    in
 
-    (* debug "add_packet_ethernet: OK"; *)
-    
-    (* debug "add_packet_ethernet: five_tuple_flow: %s" (Five_tuple_flow.to_string five_tuple_flow); *)
-
-    (* let anomaly_indice_list_ref = *)
-    (*   try *)
-    (*     ( *)
-    (*       let anomaly_indice_list = *)
-    (*         Five_tuple_flow_anomaly_indice_container_ref.find *)
-    (*           five_tuple_flow_anomaly_indice_container_ref *)
-
-    (*           (\* match_timestamps *\) *)
-
-    (*           five_tuple_flow *)
-    (*           (float_of_int packet_data_for_metrics.Packet_data_for_metrics.timestamp_sec) *)
-    (*       in *)
-
-    (*       (\* debug *\) *)
-    (*       (\*   "add_packet_ethernet: found: %s" *\) *)
-    (*       (\*   (L.to_string *\) *)
-    (*       (\*      ~sep: "\n" *\) *)
-    (*       (\*      string_of_int *\) *)
-    (*       (\*      (L.sort compare anomaly_indice_list) *\) *)
-    (*       (\*   ); *\) *)
-
-    (*       (\* debug *\) *)
-    (*       (\*   "add_packet_ethernet: found in:\n%s" *\) *)
-    (*       (\*   (Five_tuple_flow_anomaly_indice_container.to_string *\) *)
-    (*       (\*      five_tuple_flow_anomaly_indice_container *\) *)
-    (*       (\*   ); *\) *)
-
-    (*       anomaly_indice_list *)
-    (*     ) *)
-    (*   with *)
-    (*   | Not_found -> *)
-    (*     ( *)
-    (*       (\* debug "add_packet_ethernet: not_found"; *\) *)
-
-    (*       (\* debug *\) *)
-    (*       (\*   "add_packet_ethernet: not found in five_tuple_flow_anomaly_indice_container:\n%s" *\) *)
-    (*       (\*   (Five_tuple_flow_anomaly_indice_container.to_string *\) *)
-    (*       (\*      five_tuple_flow_anomaly_indice_container *\) *)
-    (*       (\*   ); *\) *)
-
-    (*       let anomaly_list_to_add = *)
-    (*         Five_tuple_flow_element_anomaly_indice_container_ref.get_anomaly_indice_list *)
-    (*           match_timestamps *)
-    (*           five_tuple_flow_element_anomaly_indice_container_ref *)
-
-    (*           five_tuple_flow *)
-    (*       in *)
-
-    (*       let anomaly_indice_list_to_add = *)
-    (*         L.map *)
-    (*           (fun anomaly -> *)
-    (*              anomaly.Admd.Instantiation.Base.Anomaly.indice *)
-    (*           ) *)
-    (*           (\* (fun (indice, _, _, _) -> *\) *)
-    (*           (\*    indice *\) *)
-    (*           (\* ) *\) *)
-    (*           anomaly_list_to_add *)
-    (*       in *)
-
-    (*       (\* debug *\) *)
-    (*       (\*   "add_packet_ethernet: anomaly_indice_list to add: %s" *\) *)
-    (*       (\*   (L.to_string *\) *)
-    (*       (\*      ~sep: " " *\) *)
-    (*       (\*      string_of_int *\) *)
-    (*       (\*      (L.sort compare anomaly_indice_list_to_add) *\) *)
-    (*       (\*   ); *\) *)
-
-    (*       if L.length anomaly_indice_list_to_add = 0 then *)
-    (*         [] *)
-    (*       else *)
-    (*         ( *)
-    (*           L.iter *)
-    (*             (fun anomaly -> *)
-    (*                let indice = anomaly.Admd.Instantiation.Base.Anomaly.indice in *)
-    (*                (\* (fun (indice, slice_l, start_time, end_time) -> *\) *)
-
-    (*                Five_tuple_flow_anomaly_indice_container_ref.add *)
-    (*                  five_tuple_flow_anomaly_indice_container_ref *)
-
-    (*                  five_tuple_flow *)
-
-    (*                  (float_of_int anomaly.Admd.Instantiation.Base.Anomaly.start_time) *)
-    (*                  (float_of_int anomaly.Admd.Instantiation.Base.Anomaly.end_time) *)
-    (*                  (\* (float_of_int start_time) *\) *)
-    (*                  (\* (float_of_int end_time) *\) *)
-    (*                  indice *)
-    (*                ; *)
-    (*             ) *)
-    (*             anomaly_list_to_add; *)
-
-    (*           (\* debug *\) *)
-    (*           (\*   "add_packet_ethernet: new five_tuple_flow_anomaly_indice_container:\n%s" *\) *)
-    (*           (\*   (Five_tuple_flow_anomaly_indice_container.to_string *\) *)
-    (*           (\*      five_tuple_flow_anomaly_indice_container *\) *)
-    (*           (\*   ); *\) *)
-
-    (*           let anomaly_indice_list = *)
-    (*             try *)
-    (*               ( *)
-    (*                 let anomaly_indice_list = *)
-    (*                   Five_tuple_flow_anomaly_indice_container_ref.find *)
-    (*                     five_tuple_flow_anomaly_indice_container_ref *)
-
-    (*                     (\* match_timestamps *\) *)
-
-    (*                     five_tuple_flow *)
-    (*                     (float_of_int packet_data_for_metrics.Packet_data_for_metrics.timestamp_sec) *)
-    (*                 in *)
-
-    (*                 anomaly_indice_list *)
-    (*               ) *)
-    (*             with *)
-    (*             | Not_found -> *)
-    (*               ( *)
-    (*                 print_endline *)
-    (*                   (sprintf *)
-    (*                      "[Anomaly_five_tuple_flow_metrics_container]: add_packet_ethernet: could not find five_tuple_flow: %s !!!!!!!!!!!!!!!!!" *)
-    (*                      (Five_tuple_flow.to_string five_tuple_flow) *)
-    (*                   ); *)
-    (*                 assert(false) *)
-    (*               ) *)
-    (*           in *)
-
-    (*           anomaly_indice_list *)
-    (*         ) *)
-    (*     ) *)
-    (* in *)
-
-    (* debug *)
-    (*   "add_packet_ethernet: anomaly_indice_list for five_tuple_flow: %s" *)
-    (*   (L.to_string *)
-    (*      ~sep: " " *)
-    (*      string_of_int *)
-    (*      (L.sort compare anomaly_indice_list) *)
-    (*   ); *)
-
-    (* L.iter *)
-    (*   (fun anomaly_indice -> *)
-    (*      try *)
-    (*        ( *)
-    (*          let five_tuple_flow_metrics_container_found = *)
-    (*            HT.find *)
-    (*              t.h *)
-    (*              anomaly_indice *)
-    (*          in *)
-
-    (*          Five_tuple_flow_metrics_container.add_packet_ethernet *)
-    (*            five_tuple_flow_metrics_container_found *)
-    (*            packet_parsing_mode *)
-    (*            packet_data_for_metrics; *)
-    (*        ) *)
-    (*      with *)
-    (*      | Not_found -> *)
-    (*        ( *)
-    (*          let five_tuple_flow_metrics_container = *)
-    (*            Five_tuple_flow_metrics_container.new_empty_t *)
-    (*              0 *)
-    (*          in *)
-    (*          Five_tuple_flow_metrics_container.add_packet_ethernet *)
-    (*            five_tuple_flow_metrics_container *)
-    (*            packet_parsing_mode *)
-    (*            packet_data_for_metrics; *)
-    (*          HT.add *)
-    (*            t.h *)
-    (*            anomaly_indice *)
-    (*            five_tuple_flow_metrics_container; *)
-
-    (*        ) *)
-    (*   ) *)
-    (*   anomaly_indice_list; *)
-
-    let anomaly_indice_list =
+    ignore(
       try
         (
-          let timestamp_sec = packet_data_for_metrics.Packet_data_for_metrics.timestamp_sec in
-          (* let timestamp_usec = packet_data_for_metrics.Packet_data_for_metrics.timestamp_usec in *)
-
-          (* debug "add_packet_ethernet: calling Five_tuple_flow_anomaly_indice_manager"; *)
-
-          let anomaly_indice_list =
-            Five_tuple_flow_anomaly_indice_manager.get_indice
-              match_timestamps
-
-              five_tuple_flow_anomaly_indice_container
-              five_tuple_flow_element_anomaly_indice_container
-
-              five_tuple_flow
-              timestamp_sec
-              0
+          let five_tuple_flow = 
+            Five_tuple_flow.of_packet_data_for_metrics
+              packet_data_for_metrics
           in
 
-          (* debug "add_packet_ethernet: OK"; *)
+          let anomaly_indice_list =
+            try
+              (
+                let timestamp_sec = packet_data_for_metrics.Packet_data_for_metrics.timestamp_sec in
+                (* let timestamp_usec = packet_data_for_metrics.Packet_data_for_metrics.timestamp_usec in *)
 
-          anomaly_indice_list
+                (* debug "add_packet_ethernet: calling Five_tuple_flow_anomaly_indice_manager"; *)
+
+                let anomaly_indice_list =
+                  Five_tuple_flow_anomaly_indice_manager.get_indice
+                    match_timestamps
+
+                    five_tuple_flow_anomaly_indice_container
+                    five_tuple_flow_element_anomaly_indice_container
+
+                    five_tuple_flow
+                    timestamp_sec
+                    0
+                in
+
+                (* debug "add_packet_ethernet: OK"; *)
+
+                anomaly_indice_list
+              )
+            with
+            | Not_found ->
+              (* debug "add_packet_ethernet: not found with Five_tuple_flow_anomaly_indice_manager"; *)
+
+              []
+          in
+
+          L.iter
+            (fun anomaly_indice ->
+               try
+                 (
+                   let five_tuple_flow_metrics_container_found =
+                     HT.find
+                       t.h
+                       anomaly_indice
+                   in
+
+                   Five_tuple_flow_metrics_container.add_packet_ethernet
+                     five_tuple_flow_metrics_container_found
+
+                     packet_parsing_mode
+                     packet_data_for_metrics;
+                 )
+               with
+               | Not_found ->
+                 (
+                   let five_tuple_flow_metrics_container =
+                     Five_tuple_flow_metrics_container.new_empty_t
+                       0
+                   in
+                   Five_tuple_flow_metrics_container.add_packet_ethernet
+                     five_tuple_flow_metrics_container
+                     packet_parsing_mode
+                     packet_data_for_metrics;
+                   HT.add
+                     t.h
+                     anomaly_indice
+                     five_tuple_flow_metrics_container;
+
+                 )
+            )
+            anomaly_indice_list;
         )
       with
-      | Not_found ->
-        (* debug "add_packet_ethernet: not found with Five_tuple_flow_anomaly_indice_manager"; *)
-
-        []
-    in
-
-    (* print_endline "test"; *)
-
-    (* if *)
-    (*   L.compare *)
-    (*     compare *)
-    (*     (L.sort compare anomaly_indice_list_ref) *)
-    (*     (L.sort compare anomaly_indice_list) *)
-    (*   <> *)
-    (*   0 *)
-    (* then *)
-    (*   ( *)
-    (*     let timestamp_sec = packet_data_for_metrics.Packet_data_for_metrics.timestamp_sec in *)
-
-    (*     Five_tuple_flow_element_anomaly_indice_container.set_debug true; *)
-
-    (*     let _anomaly_list = *)
-    (*       Five_tuple_flow_anomaly_indice_manager.get_indice *)
-    (*         match_timestamps *)
-
-    (*         five_tuple_flow_anomaly_indice_container *)
-    (*         five_tuple_flow_element_anomaly_indice_container *)
-
-    (*         five_tuple_flow *)
-    (*         timestamp_sec *)
-    (*         timestamp_sec *)
-    (*     in *)
-
-    (*     Five_tuple_flow_element_anomaly_indice_container.set_debug false; *)
-
-    (*     print_endline *)
-    (*       (sprintf *)
-    (*          "\n\ninconsistency problem for %s %d:\nreference: %s\nfrom:\n%s\n\nfound: %sfrom:\n%s\n\n" *)
-    (*          (Five_tuple_flow.to_string five_tuple_flow) *)
-
-    (*          packet_data_for_metrics.Packet_data_for_metrics.timestamp_sec *)
-
-    (*          (L.to_string string_of_int anomaly_indice_list_ref) *)
-    (*          (Five_tuple_flow_element_anomaly_indice_container_ref.to_string *)
-    (*             five_tuple_flow_element_anomaly_indice_container_ref *)
-    (*          ) *)
-
-    (*          (L.to_string string_of_int anomaly_indice_list) *)
-    (*          (Five_tuple_flow_anomaly_indice_container.to_string *)
-    (*             five_tuple_flow_anomaly_indice_container *)
-    (*          ) *)
-    (*       ) *)
-    (*     ; *)
-
-    (*     print_endline "failing"; *)
-
-    (*     failwith "problem" *)
-    (*   ); *)
-
-    (* print_endline "we are OK"; *)
-
-    L.iter
-      (fun anomaly_indice ->
-         try
-           (
-             let five_tuple_flow_metrics_container_found =
-               HT.find
-                 t.h
-                 anomaly_indice
-             in
-
-             Five_tuple_flow_metrics_container.add_packet_ethernet
-               five_tuple_flow_metrics_container_found
-
-               packet_parsing_mode
-               packet_data_for_metrics;
-           )
-         with
-         | Not_found ->
-           (
-             let five_tuple_flow_metrics_container =
-               Five_tuple_flow_metrics_container.new_empty_t
-                 0
-             in
-             Five_tuple_flow_metrics_container.add_packet_ethernet
-               five_tuple_flow_metrics_container
-               packet_parsing_mode
-               packet_data_for_metrics;
-             HT.add
-               t.h
-               anomaly_indice
-               five_tuple_flow_metrics_container;
-
-           )
-      )
-      anomaly_indice_list;
+      | Five_tuple_flow.Not_IP ->
+        (
+          ()
+        );
+    );
 
     (* debug "add_packet_ethernet: end"; *)
   )
